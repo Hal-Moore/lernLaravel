3fc76449911d6d11b20236d81657c12444590319
Hal-Moore <fulworth85@gmail.com>
2025-09-11 20:45:00 +0200

    feat: add layout template for all pages

---
+++ resources/views/Layout/main.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Worker App</h1>
+    <h1>Some header navigation</h1>
+
+    @yield('content')
+</body>
+</html>
+
+<style>
+    .my_pagination svg{
+        width: 20px;
+    }
+</style>
--- resources/views/worker/edit.blade.php
+++ resources/views/worker/edit.blade.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    Create page
-    <hr>
+@extends('layout.main')
+@section('content')
     <div>
         <form action="{{ route('worker.update', $worker->id) }}" method="POST">
             @csrf
@@ -55,5 +46,4 @@
             <div style="margin-bottom: 20px"><input type="submit" value="Сохранить"></div>
         </form>
     </div> 
-</body>
-</html>
+@endsection
